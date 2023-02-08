131195c6e4fa6716668f88413ed95b51c1f7fa2b
sekheng <leesekheng@yahoo.com>
2023-02-08 15:46:13 +0800

    Trying to see if something changes

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>IR Group 32</h1>
+            <p>Start editing to see some magic happen :)</p>
+        </div>
+    );
 }
