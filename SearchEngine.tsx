1eb8ca512f6ca495ed4c8660257fc11055666491
sekheng <leesekheng@yahoo.com>
2023-02-09 23:03:46 +0800

    added a theoretical search engine

---
+++ SearchEngine.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function SearchEngine() {
+    const [data, setData] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        axios
+            .get('https://api.example.com/data')
+            .then((response) => {
+                setData(response.data);
+                setIsLoading(false);
+            })
+            .catch((error) => {
+                console.error(error);
+                setIsLoading(false);
+            });
+    }, []);
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredData = data.filter((item) =>
+        item.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return (
+        <div>
+            <input type="text" value={searchTerm} onChange={handleChange} />
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <ul>
+                    {filteredData.map((item) => (
+                        <li key={item.id}>{item.title}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export default SearchEngine;
---
+++ SearchForm.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+class SearchForm extends React.Component {}
