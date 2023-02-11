1b9b9a100031cf841421a0e56224b25ac8dcec2c
sekheng <leesekheng@yahoo.com>
2023-02-11 23:37:14 +0800

    took a while to figure out a search form

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import './style.css';
+import SearchForm from './SearchForm';
 
 export default function App() {
     return (
         <div>
             <h1>IR Group 32</h1>
-            
+            <SearchForm />
         </div>
     );
 }
--- SearchForm.tsx
+++ SearchForm.tsx
@@ -1,3 +1,43 @@
 import * as React from 'react';
 
-class SearchForm extends React.Component {}
+interface SearchFState {
+    message: string;
+}
+
+class SearchForm extends React.Component<{}, SearchFState> {
+    state = {
+        message: '',
+    };
+
+    handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(`Name: ${this.state.message}`);
+    };
+
+    handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value } as Pick<
+            SearchFState,
+            keyof SearchFState
+        >);
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <input
+                        type="text"
+                        id="message"
+                        name="message"
+                        value={this.state.message}
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default SearchForm;
